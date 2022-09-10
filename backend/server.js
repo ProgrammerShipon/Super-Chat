4aefd0df473333d1efa1f12e48b00a8ca1493f93
Shipon Hossen Raju <msshipon234@gmail.com>
2022-09-11 01:45:09 +0600

    Back-end setup and api test

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+console.clear();
+
+// Get Express
+const express = require('express');
+const chats = require('./data/data');
+
+// Express instance 
+const app = express ();
+
+// dot env / .env
+const dotEnv = require('dotenv');
+dotEnv.config();
+
+// Routs 
+app.get('/', (req, res) => {
+   res.send('API is Running Successfully');
+})
+app.get('/api/chat', (req, res) => {
+   res.send(chats);
+})
+app.get('/api/chat/:id', (req, res) => {
+   // res.send('Sayed');
+   res.send(req.params.id);
+   const singleChat = chats.find((n) => n._id === req.params.id);
+   console.log(singleChat);
+})
+// app.get('/api/chat:id', (req, res) => {
+//    const singleChat = chats.find((c) => c._id);
+//    res.send(singleChat);
+// })
+
+
+
+// Port mangement
+const PORT = process.env.PORT || 5000
+// Server 
+app.listen(5000, console.log(`Server Started on PORT  ${PORT}`));
+
