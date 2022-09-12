ffde20494db1102352f5a7c5ccf7a9543dd669f5
Shipon Hossen Raju <msshipon234@gmail.com>
2022-09-12 08:10:43 +0600

    mongoose install

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
